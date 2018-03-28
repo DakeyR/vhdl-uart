789ecbe0f6f2d27f5aa4a9142ecea16b2a6ab102
Francois Mazeau <mazeau_f@epita.fr>
2018-03-28 17:36:35 +0200

    fdiv: add first trial version

--- src/fdiv.vhd
+++ src/fdiv.vhd
@@ -1,119 +1,119 @@
 -- FDIV.vhd
 -- ---------------------------------------
---   Frequency Divider for DE0
+--   Frequency Divider
 -- ---------------------------------------
+
+-- Baudrates:
 --
--- Note : Only Tick1ms is used in this exercise.
---        you can leave the other outputs unconnected.
+-- 9600   = 104us
+-- 19200  = 52us
+-- 56000  = 18us
+-- 115200 = 9us
+
 
 LIBRARY ieee;
   USE ieee.std_logic_1164.ALL;
   USE ieee.numeric_std.ALL;
 
 -- ---------------------------------------
-    Entity FDIV is
--- ---------------------------------------
-   Generic (  Fclock : positive := 50E6); -- System Clock Freq in Hertz
-      Port (     CLK : In  std_logic;
-                 RST : In  std_logic;
-             Tick1us : Out std_logic;
-             Tick4us : Out std_logic;
-            Tick10us : Out std_logic;
-             Tick1ms : Out std_logic;
-            Tick10ms : Out std_logic  );
+Entity FDIV is
+   -- ---------------------------------------
+    Generic (  Fclock : positive := 50E6); -- System Clock Freq in Hertz
+    Port (     CLK : in  std_logic;
+               RST : in  std_logic;
+               Tick9us : out std_logic;
+               Tick18us : out std_logic;
+               Tick52us : out std_logic;
+               Tick104us : out std_logic);
 end FDIV;
 
--- ---------------------------------------
-    Architecture RTL of FDIV is
+    -- ---------------------------------------
+Architecture RTL of FDIV is
 -- ---------------------------------------
 
-constant Divisor_us : positive := Fclock  / 1E6;
-signal Count1     : integer range 0 to Divisor_us;
-signal Count3     : integer range 0 to 9;
-signal Count2     : integer range 0 to 100;
-signal Count4     : integer range 0 to 9;
-signal Count5     : integer range 0 to 9;
-signal Tick1us_i  : std_logic;
-signal Tick4us_i  : std_logic;
-signal Tick10us_i : std_logic;
-signal Tick1ms_i  : std_logic;
-signal Tick10ms_i  : std_logic;
+    constant Divisor_us : positive := Fclock  / 1E6;
+    signal Count1       : integer range 0 to Divisor_us;
+    signal Count9       : integer range 0 to 8;
+    signal Count18      : integer range 0 to 1;
+    signal Count52      : integer range 0 to 51;
+    signal Count104     : integer range 0 to 1;
+    signal Tick1us_i     : std_logic;
+    signal Tick9us_i     : std_logic;
+    signal Tick18us_i   : std_logic;
+    signal Tick52us_i   : std_logic;
+    signal Tick104us_i  : std_logic;
 
 -- -------------------------------------
 
 begin
 
-Tick1us  <= Tick1us_i;
-Tick4us  <= Tick4us_i;
-Tick10us <= Tick10us_i;
-Tick1ms  <= Tick1ms_i;
-Tick10ms <= Tick10ms_i;
+    Tick9us  <= Tick9us_i;
+    Tick18us  <= Tick18us_i;
+    Tick52us <= Tick52us_i;
+    Tick104us <= Tick104us_i;
 
-process (RST,CLK)
-begin
-  if RST='1' then
-    Count1     <= 0;
-    Count2     <= 0;
-    Count3     <= 0;
-    Count4     <= 0;
-    Count5     <= 0;
-    Tick1us_i  <= '0';
-    Tick4us_i  <= '0';
-    Tick10us_i <= '0';
-    Tick1ms_i  <= '0';
-    Tick10ms_i <= '0';
+    process (RST,CLK)
+    begin
+        if RST='1' then
+            Count1      <= 0;
+            Count9      <= 0;
+            Count18     <= 0;
+            Count52     <= 0;
+            Count104    <= 0;
+            Tick1us_i   <= '0';
+            Tick9us_i   <= '0';
+            Tick18us_i  <= '0';
+            Tick52us_i  <= '0';
+            Tick104us_i <= '0';
 
-  elsif rising_edge (CLK) then
+        elsif rising_edge (CLK) then
+            Tick1us_i <= '0';
+            Tick9us_i <= '0';
+            Tick18us_i <= '0';
+            Tick52us_i <= '0';
+            Tick104us_i <= '0';
 
-    Tick1us_i  <= '0';
-    Tick4us_i  <= '0';
-    Tick10us_i <= '0';
-    Tick1ms_i  <= '0';
-    Tick10ms_i <= '0';
+            if Count1 < Divisor_us-1 then
+                Count1 <= Count1 + 1;
+            else
+                Count1 <= 0;
+                Tick1us_i <= '1';
+            end if;
 
-    if Count1 < Divisor_us-1 then
-      Count1 <= Count1 + 1;
-    else
-      Count1 <= 0;
-      Tick1us_i <= '1';
-    end if;
+            if Tick1us_i='1' then
+                if Count9 < 8 then
+                    Count9 <= Count9 + 1;
+                else
+                    Count9 <= 0;
+                    Tick9us_i <= '1';
+                end if;
 
-    if Tick1us_i='1' then
-      if Count3 < 9 then
-        Count3 <= Count3 + 1;
-      else
-        Count3 <= 0;
-        Tick10us_i <= '1';
-      end if;
-    end if;
+                if Count52 < 51 then
+                    Count52 <= Count52 + 1;
+                else
+                    Count52 <= 0;
+                    Tick52us_i <= '1';
+                end if;
+            end if;
 
-    if Tick1us_i='1' then
-      if Count4 < 3 then
-        Count4 <= Count4 + 1;
-      else
-        Count4 <= 0;
-        Tick4us_i <= '1';
-      end if;
-    end if;
+            if Tick9us_i = '1' then
+                if Count18 = 0 then
+                    Count18 <= 1;
+                else
+                    Count18 <= 0;
+                    Tick18us_i <= '1';
+                end if;
+            end if;
 
-    if Tick10us_i='1' then
-      if Count2 < 99  then
-        Count2 <= Count2 + 1;
-      else
-        Count2 <= 0;
-        Tick1ms_i <= '1';
-        if Count5=9 then
-          Tick10ms_i <= '1';
-          Count5 <= 0;
-        else
-          Count5 <= Count5 + 1;
+            if Tick52us_i = '1' then
+                if Count104 = 0 then
+                    Count104 <= 1;
+                else
+                    Count104 <= 0;
+                    Tick104us_i <= '1';
+                end if;
+            end if;
         end if;
-      end if;
-    end if;
-
-
-  end if;
-end process;
-
+    end process;
 end RTL;
 
